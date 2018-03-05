a239a081fc9171374b33b4c0a1f73412a19bcca0
SALLE David <david.salle@ensemblescolaire-niort.com>
2018-03-05 17:02:52 +0100

    Add multi-clients

--- server.go
+++ server.go
@@ -30,6 +30,13 @@ var upgrader = websocket.Upgrader{
 }
 
 
+// Variables globales
+var PID = 0
+var UID = ""
+var NbClients = 0
+var Clients [4]*websocket.Conn
+
+
 // Fonction pour lancer un script Python dans un nouveau processus
 func writePythonScript(code string) {
 	fmt.Println(" => Ecriture du script : ", code)
@@ -40,7 +47,7 @@ func writePythonScript(code string) {
 	// Enregistrement du code source dans un script
 	err := ioutil.WriteFile("todo.py", octets, 0644)
 	if err != nil {
-		fmt.Println("ERROR with WriteFile() => ", err)
+		fmt.Println(" => ERROR with WriteFile() :", err)
 	}
 }
 
@@ -66,21 +73,58 @@ func executePythonScript() string {
 }
 
 
-// Fonction pour gérer les websockets
-func WebSockethandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer conn.Close()
+// Fonction pour envoyer une reponse sur la websocket
+func sendToOneResponse(conn *websocket.Conn, s string, a string, d string, p int, u string, c int) bool {
+	// Préparation de la trame à envoyer
+	var reponse Trame
+	reponse.Source = s
+	reponse.Action = a
+	reponse.Details = d
+	reponse.ExecPID = p
+	reponse.ExecUID = u
+	reponse.Clients = c
 
+	// Envoi de la trame préparée
+	err := conn.WriteJSON(reponse)
+	if err != nil {
+		fmt.Println("=> ERROR with WriteJSON() :", err)
+		return false
+	}
+
+	return true
+}
+
+
+// Fonction pour envoyer une reponse sur la websocket
+func sendToAllResponse(s string, a string, d string, p int, u string, c int) {
+	for _, conn := range Clients {
+        sendToOneResponse(conn, s, a, d, p, u, c)
+    }
+}
+
+
+// Fonction pour ajouter un client à la liste
+func addClient(conn *websocket.Conn) {
+	Clients[NbClients] = conn
+	NbClients++
+	sendToAllResponse("robot", "informer", "clients", 0, "", NbClients)
+}
+
+
+// Fonction pour supprimer un client de la liste
+func removeClient(conn *websocket.Conn) {
+	conn.Close()
+}
+
+
+// Fonction pour gérer individuellement chaque connexion des clients
+func ClientHandler(conn *websocket.Conn) {
 	for {
 		// Lecture et décodage des données JSON sur la websocket
 		requete := Trame{}
 		err := conn.ReadJSON(&requete)
 		if err != nil {
-			fmt.Println("ERROR with ReadJSON() => ", err)
+			fmt.Println("=> ERROR with ReadJSON() :", err)
 			break
 		}
 		fmt.Println(" => WebSockethandler :")
@@ -93,42 +137,49 @@ func WebSockethandler(w http.ResponseWriter, r *http.Request) {
 
 		// Choix selon l'action à effectuer
 		if (requete.Action == "lancer") {
-			// Ecriture du code dans un script Python
-			writePythonScript(requete.Details)
+			// On teste si le robot est libre d'exécution
+			if (PID == 0) {
+				// On confirme le lancement de l'exécution
+				sendToOneResponse(conn, "robot", "informer", "starting", 0, "", NbClients)
 
-			// Exécution du script fraichement créé
-			resultat := executePythonScript()
+				// Ecriture du code dans un script Python
+				writePythonScript(requete.Details)
 
-			// Réponse renvoyée sur la websocket
-			var reponse Trame
-			reponse.Source = "robot"
-			reponse.Action = "informer"
-			reponse.Details = resultat
-			reponse.ExecPID = 0
-			reponse.ExecUID = ""
-			reponse.Clients = 0
-			err = conn.WriteJSON(reponse)
-			if err != nil {
-				fmt.Println("ERROR with WriteJSON() => ", err)
-				break
+				// Exécution du script fraichement créé
+				resultat := executePythonScript()
+
+				// Réponse renvoyée sur la websocket
+				sendToOneResponse(conn, "robot", "informer", resultat, 0, "", NbClients)
 			}
 		} else if (requete.Action == "arreter") {
 
 		}
 	}
+
+	// Fermeture de la connexion
+	//conn.Close()
 }
 
 
-/*
-// Fonction pour servir la page gérant l'IDE
-func IdeFileHandler(response http.ResponseWriter, request *http.Request) {
-    fmt.Println(" => Requête pour : ", request.URL.Path[1:])
+// Fonction pour gérer les websockets (https://gist.github.com/tmichel/7390690)
+func WebsocketHandler(w http.ResponseWriter, r *http.Request) {
+	// Attente de connexion d'un client
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer removeClient(conn)
 
-	tmpl := template.Must(template.ParseFiles("ide.html"))
-    data :=
-    tmpl.Execute(response, data)
+	// Ajout de la connexion du client au tableau
+	Clients[NbClients] = conn
+	NbClients++
+	sendToAllResponse("robot", "informer", "clients", 0, "", NbClients)
+	//sendResponse(conn, "robot", "informer", "clients", 0, "", NbClients)
+
+	// Lancement d'un thread pour gérer la connexion
+	go ClientHandler(conn)
 }
-*/
 
 
 // Fonction pour servir les pages statiques (html, css et js)
@@ -147,7 +198,7 @@ func main() {
 	fmt.Println(" => Lancement en cours...")
 
 	// Enregistrement de la fonction gérant les websockets
-	http.HandleFunc("/ws", WebSockethandler)
+	http.HandleFunc("/ws", WebsocketHandler)
 
 	// Enregistrement de la fonction gérant les pages statiques (IMG, CSS, JS...)
 	http.HandleFunc("/", StaticFileHandler)
